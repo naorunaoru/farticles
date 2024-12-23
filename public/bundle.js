ba17542fe0c3d4ddae15e525638dd46d3929611d
Roman Kuraev <naoru.desu@gmail.com>
2024-12-23 16:26:32 +0000

    poop

--- public/bundle.js
+++ public/bundle.js
@@ -1,5 +1,3 @@
-
-(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 (function () {
   'use strict';
 
@@ -6263,4 +6261,3 @@
   });
 
 })();
-//# sourceMappingURL=bundle.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,18 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
 
+const production = !process.env.ROLLUP_WATCH;
+
 export default {
   input: "src/main.js",
   output: {
     file: "public/bundle.js",
     format: "iife",
-    sourcemap: true,
+    sourcemap: !production,
   },
-  plugins: [nodeResolve(), serve("public"), livereload("public")],
+  plugins: [
+    nodeResolve(),
+    !production && serve("public"),
+    !production && livereload("public"),
+  ].filter(Boolean),
 };
